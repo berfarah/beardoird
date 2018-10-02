f6f2f135f1416a3348120c04b166d64ea052a8af
Bernardo Farah <ber@bernardo.me>
2018-10-02 01:28:43 -0700

    --wip-- [skip ci]

--- main.go
+++ main.go
@@ -4,10 +4,11 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/berfarah/beardroid/plugins/inventory/uniqlo"
 	"github.com/botopolis/bot"
 	"github.com/botopolis/redis"
 	"github.com/botopolis/slack"
+	"github.com/botopolis/slack/action"
+	oslack "github.com/nlopes/slack"
 )
 
 func main() {
@@ -15,13 +16,45 @@ func main() {
 	r := bot.New(
 		adapter,
 		redis.New(os.Getenv("REDIS_URL")),
-		&uniqlo.Plugin{},
+		action.New("/interaction", os.Getenv("SLACK_SIGNATURE")),
 	)
+
+	var a action.Plugin
+	var slk slack.Adapter
+	r.Plugin(&a)
+	r.Plugin(&slk)
+	a.Add("example", func(cb oslack.AttachmentActionCallback) {
+		slk.Client.UpdateMessage(cb.Channel.ID, cb.MessageTs, "updatered")
+	})
+
+	r.Hear(bot.Contains("trigger"), func(r bot.Responder) error {
+		err := r.Send(bot.Message{
+			Params: oslack.PostMessageParameters{
+				Attachments: []oslack.Attachment{{
+					Text:       "Trigger example",
+					CallbackID: "example",
+					Actions: []oslack.AttachmentAction{{
+						Name:  "check",
+						Type:  "button",
+						Text:  "Do it",
+						Value: "true",
+					}, {
+						Name:  "check",
+						Type:  "button",
+						Text:  "Nah",
+						Style: "danger",
+						Value: "false",
+					}},
+				}},
+			},
+		})
+		r.Logger.Error("NOPE: %v", err)
+		return err
+	})
 
 	r.Router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World"))
 	}).Methods("GET")
 
-	r.Debug(true)
 	r.Run()
 }
